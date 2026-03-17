0f70e143e92a68d084fd59cba1a790ee5a8113d6
Andreas Molander <andreas.molander@cern.ch>
2026-03-17 11:16:41 +0200

    Add FITExtras table

---
+++ Common/DataModel/FITExtra.h
@@ -0,0 +1,57 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#ifndef COMMON_DATAMODEL_FITEXTRA_H_
+#define COMMON_DATAMODEL_FITEXTRA_H_
+
+#include "Framework/ASoA.h"
+#include "Framework/AnalysisDataModel.h"
+
+namespace o2::aod
+{
+namespace fit
+{
+// Quantities copied straight from AOD
+DECLARE_SOA_COLUMN(PV, pv, float);                     //! Primary vertex position in cm (o2::aod::‌collision::PosZ)
+DECLARE_SOA_COLUMN(NContrib, nContrib, int);           //! Number of contributors to primary vertex (o2::aod::‌collision::NumContrib)
+DECLARE_SOA_COLUMN(FT0TimeA, ft0timeA, float);         //! FT0-A average time in ns (o2::aod::ft0::TimeA)
+DECLARE_SOA_COLUMN(FT0TimeC, ft0timeC, float);         //! FT0-C average time in ns (o2::aod::ft0::TimeC)
+DECLARE_SOA_COLUMN(FT0TimeACorr, ft0timeACorr, float); //! FT0-A average time in ns corrected PV (o2::aod::ft0::T0ACorrected)
+DECLARE_SOA_COLUMN(FT0TimeCCorr, ft0timeCCorr, float); //! FT0-C average time in ns corrected PV (o2::aod::ft0::T0CCorrected)
+DECLARE_SOA_COLUMN(FT0Time, ft0time, float);           //! FT0 collision time in ns (o2::aod::ft0::CollTime)
+DECLARE_SOA_COLUMN(FT0TimeRes, ft0timeRes, float);     //! FT0 collision time resolution in ns (o2::aod::ft0::T0Resolution)
+DECLARE_SOA_COLUMN(FT0Vtx, ft0vtx, float);             //! FT0 vertex in cm (o2::aod::ft0::PosZ)
+DECLARE_SOA_COLUMN(FV0Time, fv0time, float);           //! FV0 average time in ns (o2::aod::fv0a::Time)
+DECLARE_SOA_COLUMN(FDDTimeA, fddtimeA, float);         //! FDD-A average time in ns (o2::aod::fdd::TimeA)
+DECLARE_SOA_COLUMN(FDDTimeC, fddtimeC, float);         //! FDD-C average time in ns (o2::aod::fdd::TimeC)
+
+// Derived quantities
+
+// Event selection conditions straigt from AOD
+DECLARE_SOA_COLUMN(Sel8, sel8, bool);                  //! (o2::aod::evsel::Sel8)
+DECLARE_SOA_COLUMN(HasFT0, hasFT0, bool);              //! (o2::aod::collision::has_foundFT0())
+DECLARE_SOA_COLUMN(HasFV0, hasFV0, bool);              //! (o2::aod::collision::has_foundFV0())
+DECLARE_SOA_COLUMN(HasFDD, hasFDD, bool);              //! (o2::aod::collision::has_foundFDD())
+DECLARE_SOA_COLUMN(FT0Triggers, ft0Triggers, uint8_t); //! FT0 trigger mask
+DECLARE_SOA_COLUMN(FV0Triggers, fv0Triggers, uint8_t); //! FV0 trigger mask
+DECLARE_SOA_COLUMN(FDDTriggers, fddTriggers, uint8_t); //! FDD trigger mask
+} // namespace fit
+DECLARE_SOA_TABLE(FITExtras, "AOD", "FITEXTRA", //! Table with extra FIT information
+                  fit::Sel8, fit::HasFT0, fit::HasFV0, fit::HasFDD,
+                  fit::FT0Triggers, fit::FV0Triggers, fit::FDDTriggers,
+                  fit::PV, fit::NContrib,
+                  fit::FT0TimeA, fit::FT0TimeC, fit::FT0TimeACorr, fit::FT0TimeCCorr,
+                  fit::FT0Time, fit::FT0TimeRes, fit::FT0Vtx,
+                  fit::FV0Time, fit::FDDTimeA, fit::FDDTimeC);
+using FITExtra = FITExtras::iterator;
+} // namespace o2::aod
+
+#endif // COMMON_DATAMODEL_FITEXTRA_H_
---
+++ Common/TableProducer/fitExtraTable.cxx
@@ -0,0 +1,130 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#include "Common/DataModel/EventSelection.h"
+#include "Common/DataModel/FITExtra.h"
+#include "Common/DataModel/FT0Corrected.h"
+
+#include <CommonConstants/PhysicsConstants.h>
+#include <Framework/AnalysisDataModel.h>
+#include <Framework/AnalysisTask.h>
+
+using namespace o2;
+using namespace o2::framework;
+
+#include "Framework/runDataProcessing.h"
+
+struct fitExtraTable {
+  // Producer
+  Produces<o2::aod::FITExtra> table;
+
+  // using CollisionEvSel = soa::Join<aod::Collisions, aod::EvSels>::iterator;
+  static constexpr float invLightSpeedCm2NS = 1.f / o2::constants::physics::LightSpeedCm2NS;
+
+  void init(InitContext const&)
+  {
+  }
+  
+  void process(soa::Join<aod::Collisions, aod::EvSels, aod::FT0sCorrected> const& collisions,
+               aod::FT0s const&, aod::FV0As const&, aod::FDDs const&) {
+    table.reserve(collisions.size());
+
+    float pv = -200;
+    int nContrib = -1;
+
+    float ft0timeA = -200;
+    float ft0timeC = -200;
+    float ft0timeACorr = -200;
+    float ft0timeCCorr = -200;
+    float ft0time = -200;
+    float ft0timeRes = -200;
+    float ft0vtx = -200;
+    float fv0time = -200;
+    float fddtimeA = -200;
+    float fddtimeC = -200;
+
+    bool sel8, hasFT0, hasFV0, hasFDD;
+    uint8_t ft0Triggers, fv0Triggers, fddTriggers;
+
+    for (const auto& collision : collisions) {
+      pv = collision.posZ();
+      nContrib = collision.numContrib();
+      sel8 = collision.sel8();
+
+      ft0timeA = -200;
+      ft0timeC = -200;
+      ft0timeACorr = -200;
+      ft0timeCCorr = -200;
+      ft0time = -200;
+      ft0timeRes = -200;
+      ft0vtx = -200;
+      fv0time = -200;
+      fddtimeA = -200;
+      fddtimeC = -200;
+
+      hasFT0 = collision.has_foundFT0();
+      hasFV0 = collision.has_foundFV0();
+      hasFDD = collision.has_foundFDD();
+      ft0Triggers = 0;
+      fv0Triggers = 0;
+      fddTriggers = 0;
+
+      if (hasFT0) {
+        auto ft0 = collision.foundFT0();
+        ft0timeA = ft0.timeA();
+        ft0timeC = ft0.timeC();
+        ft0time = ft0.collTime();
+        ft0vtx = ft0.posZ();
+        ft0timeACorr = collision.t0ACorrected();
+        ft0timeCCorr = collision.t0CCorrected();
+        ft0timeRes = collision.t0resolution();
+        ft0Triggers = ft0.triggerMask();
+      }
+      if (hasFV0) {
+        auto fv0 = collision.foundFV0();
+        fv0time = fv0.time();
+        fv0Triggers = fv0.triggerMask();
+      }
+      if (hasFDD) {
+        auto fdd = collision.foundFDD();
+        fddtimeA = fdd.timeA();
+        fddtimeC = fdd.timeC();
+        fddTriggers = fdd.triggerMask();
+      }
+  
+      table(pv,
+            nContrib,
+            ft0timeA,
+            ft0timeC,
+            ft0timeACorr,
+            ft0timeCCorr,
+            ft0time,
+            ft0timeRes,
+            ft0vtx,
+            fv0time,
+            fddtimeA,
+            fddtimeC,
+            sel8,
+            hasFT0,
+            hasFV0,
+            hasFDD,
+            ft0Triggers,
+            fv0Triggers,
+            fddTriggers);
+    }
+  }
+};
+
+WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
+{
+  WorkflowSpec workflow{adaptAnalysisTask<fitExtraTable>(cfgc)};
+  return workflow;
+}
